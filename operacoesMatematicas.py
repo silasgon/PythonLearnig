31ab8acf7c761b980dbd56eae14e0f9a72749cc7
silasgon <silasidc@gmail.com>
2018-10-01 15:20:07 -0300

    teste

---
+++ operacoesMatematicas.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite outro numero: "))
+
+divisao = num1 / num2
+resto = num1 % num2
+print()
+print(num1, "Dividido por", num2, "é igual a : ", divisao)
+
+print("O resto da divisão entre", num1, "e", num2, "é igual a: ", resto)
