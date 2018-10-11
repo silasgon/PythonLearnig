f86659b517ce4c039ff59118c1bfed8d321d574f
silasgon <silasidc@gmail.com>
2018-10-11 18:04:49 -0300

    teste 11

--- aula47.py
+++ aula47.py
@@ -1,6 +1,12 @@
-lista = [10,200,300,400]
+# lista = [10,200,300,400,500,600,700,800]
+# #
+# #
+# # for item in range(len(lista)):
+# #     lista[item] += 1000
+# # print(lista)
 
-lista_indice = [0,1,2,3]
+lista  = [10,200,300,400,500,600,700,800]
 
-for item in lista_indice:
-    
+for idx,item in enumerate(lista):
+    lista[idx] += 1000
+print(lista)
